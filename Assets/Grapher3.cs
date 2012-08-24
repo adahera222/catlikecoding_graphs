f2dfe7c3c1dec20aebfe8aa6843fc9bbabf558b9
David Lusby <lusbyd@gmail.com>
2012-08-24 22:23:02 +0100

    Update Sine too. We transform it into eight blobs by multiplying the square sines of X, Y, and Z together. We only animate the Z-based sine, but we make a distinction here. The top and bottom half of the graph will move in opposite directions.
    Produces a "marching blobs" effect

--- Assets/Grapher3.cs
+++ Assets/Grapher3.cs
@@ -89,10 +89,10 @@ public class Grapher3 : MonoBehaviour {
 	}
 
 	private static float Sine (Vector3 p, float t) {
-		return 0.50f +
-			0.25f * Mathf.Sin(4 * Mathf.PI * p.x + 4 * t) * Mathf.Sin(2 * Mathf.PI * p.z + t) +
-			0.10f * Mathf.Cos(3 * Mathf.PI * p.x + 5 * t) * Mathf.Cos(5 * Mathf.PI * p.z + 3 * t) +
-			0.15f * Mathf.Sin(Mathf.PI * p.x + 0.6f * t);
+		float x = Mathf.Sin(2 * Mathf.PI * p.x);
+		float y = Mathf.Sin(2 * Mathf.PI * p.y);
+		float z = Mathf.Sin(2 * Mathf.PI * p.z + (p.y > 0.5f ? t : -t));
+		return x * x * y * y * z * z;
 	}
 	
 	private static float Ripple (Vector3 p, float t) {
